34d1364987522a948cb13cf375e08ffa0ad2fab3
Benjamin Demetz <benjamin.demetz@student.tugraz.at>
2024-04-29 18:48:19 +0200

    Fixed potion and email

--- items/Potion.hpp
+++ items/Potion.hpp
@@ -11,16 +11,48 @@
 //
 
 #include "Item.hpp"
+#include "../utility/Dice.cpp"
 
 #ifndef POTION_HPP
 #define POTION_HPP
 
 enum class Effect {HEALTH, FIRE, COLD, FORCE, ACID};
 
-
 class Potion: public Item
 {
  Effect effect_;
+  public:
+    ///----------------------------------------------------------------------------------------------------------------
+    ///
+    /// Constructor for a potion.
+    /// @param abbreviation The abbreviation of the potion.
+    /// @param name The name of the potion.
+    /// @param dice The dice of the potion. can be null on resistance potions.
+    /// @param effect The effect of the potion.
+    //
+    Potion(std::string abbreviation, std::string name, Dice dice, Effect effect);
+    ///----------------------------------------------------------------------------------------------------------------
+    ///
+    /// Getter for the effect of the potion.
+    ///
+    /// @return The effect of the potion.
+    //
+    Effect getEffect() const;
+    ///----------------------------------------------------------------------------------------------------------------
+    ///
+    /// Deleted copy constructor.
+    //
+    Potion(const Potion&) = delete;
+    ///----------------------------------------------------------------------------------------------------------------
+    ///
+    /// Deleted assignment operator.
+    //
+    Potion& operator=(const Potion&) = delete;
+    ///----------------------------------------------------------------------------------------------------------------
+    ///
+    /// Destructor for a potion. Default.
+    //
+    virtual ~Potion() = default;
 };
 
 
