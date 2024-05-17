deef910ffa3bdc2ec10591e2d1a5c050240b0b94
Benjamin Demetz <benjamin.demetz@student.tugraz.at>
2024-05-17 16:30:37 +0200

    Update Enemy.cpp

--- entity/character/Enemy.cpp
+++ entity/character/Enemy.cpp
@@ -48,7 +48,8 @@ void Enemy::initializeInventory()
     weapon_ = Props::craftWeapon("DAGG", strength_, vitality_);
     armor_ = nullptr;
     loot_ = {{"DAGG", 1}};
-    inventory_ = nullptr;
+    inventory_ = std::make_shared<Inventory>();
+    inventory_->addWeapon(weapon_);
   }
   else if (abbreviation_ == 'G')
   {
