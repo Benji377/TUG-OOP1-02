d56b137b4d0b618468301c5dfe7f05d64e3a2e5d
Hanno Elsnig <hanno.elsnig@student.tugraz.at>
2024-05-17 19:06:06 +0200

    Printed Death Location loot

--- commands/Command.cpp
+++ commands/Command.cpp
@@ -303,12 +303,7 @@ void LootCommand::execute(std::vector<std::string> params)
   std::shared_ptr<Field> field = game_->getCurrentRoom()->getField(target_position);
   std::shared_ptr<Entity> entity_on_field = field->getEntity();
 
-  if(std::dynamic_pointer_cast<DeathLocation>(entity_on_field) != nullptr)
-  {
-    game_->getDungeon().lootEntity(player, entity_on_field);
-    game_->plusOneActionCount();
-  }
-  else if(std::dynamic_pointer_cast<TreasureChest>(entity_on_field) != nullptr)
+  if(std::dynamic_pointer_cast<TreasureChest>(entity_on_field) != nullptr)
   {
     std::shared_ptr<TreasureChest> chest = std::dynamic_pointer_cast<TreasureChest>(entity_on_field);
 
@@ -325,27 +320,26 @@ void LootCommand::execute(std::vector<std::string> params)
     if(roll_result < min_value_to_roll)
     {
       std::cout << game_->getStory().getStorySegment("N_LOOT_CHEST_LOCKED");
-    }
-    else
-    {
-      std::cout << game_->getStory().getStorySegment("N_LOOT_SUCCESSFUL");
-
-      std::map<string, int> loot = entity_on_field->getLoot();
-      std::shared_ptr<Inventory> inv_of_entity = std::make_shared<Inventory>();
-      int ret = inv_of_entity->parseInventory(loot);
-      if (ret == 1)
-      {
-        std::cout << "The entity contains an unknown item. The loot could not be parsed." << std::endl;
-      }
-      IO::printInventory(inv_of_entity, nullptr);
-      game_->getDungeon().lootEntity(player, entity_on_field);
-      game_->plusOneActionCount();
+      return;
     }
   }
-  else
+  else if(std::dynamic_pointer_cast<DeathLocation>(entity_on_field) == nullptr)
   {
     throw InvalidPositionCommand();
   }
+
+  std::cout << game_->getStory().getStorySegment("N_LOOT_SUCCESSFUL");
+
+  std::map<string, int> loot = entity_on_field->getLoot();
+  std::shared_ptr<Inventory> inv_of_entity = std::make_shared<Inventory>();
+  int ret = inv_of_entity->parseInventory(loot);
+  if (ret == 1)
+  {
+    std::cout << "The entity contains an unknown item. The loot could not be parsed." << std::endl;
+  }
+  IO::printInventory(inv_of_entity, nullptr);
+  game_->getDungeon().lootEntity(player, entity_on_field);
+  game_->plusOneActionCount();
 
 }
 
