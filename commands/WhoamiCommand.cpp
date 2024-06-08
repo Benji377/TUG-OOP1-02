b2d412bc322ec9de0aba693a9040bf06e8475803
Benjamin Demetz <benjamin.demetz@student.tugraz.at>
2024-06-08 18:53:46 +0200

    Update WhoamiCommand.cpp

--- commands/WhoamiCommand.cpp
+++ commands/WhoamiCommand.cpp
@@ -5,6 +5,6 @@ void WhoamiCommand::execute(std::vector<std::string> params)
 {
   checkCommandLenght(params, 1);
 
-  std::cout << "Insert Team Name" << std::endl;
+  std::cout << "The Q-If-Else-ers" << std::endl;
 
 }
